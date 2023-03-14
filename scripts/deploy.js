1d9696cad8207c66829d11ffa97cbef69e0245f7
kasodesyn <bokang.qian@desyn.io>
2023-03-14 22:51:44 +0800

    add deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Nextme = await hre.ethers.getContractFactory("Nextme");
+  const nextme = await Nextme.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await nextme.deployed();
+  console.log(`deployed to ${nextme.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
